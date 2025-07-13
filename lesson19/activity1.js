da2cddfbe737509c6cfe3ececdb0b8b48488c7ca
Bhargavglitch <bhargavsaravanan7@gmail.com>
2025-07-13 21:59:28 +0530

    lesson completed

---
+++ lesson19/activity1.js
@@ -0,0 +1,15 @@
+<!docktype html>
+<html>
+    <body>
+        <h1 id="myH"></h1>
+        <p1 id="myP"></p1>
+        <script>
+            //My heading:
+            document.getElementById("myH").innerHTML = "javaScript"
+            Comments";
+            // my paragraph:
+            document.getElementById("myP").innerHTML = "my first"
+            paragraph.";
+        </script>
+    </body>
+</html>
