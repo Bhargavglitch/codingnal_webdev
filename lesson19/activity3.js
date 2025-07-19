b2169f8152188d880f286d7c87fb467b72c65578
Bhargavglitch <bhargavsaravanan7@gmail.com>
2025-07-19 22:05:59 +0530

    lesson 20 completed

---
+++ lesson19/activity3.js
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <h2>Javascript statements</h2>
+        <p>A <b>javascript program</b>is a list of <b>statements</b>
+        to be ecuted by a computer.</p>
+        <p id="demo"></p>
+        <script>
+            var x,y,z //statement 1
+            x=5;      //statement2
+            y=6;      //statement3
+            z=x+y;    //statement4
+
+            document.getElementsId("deno").innerHTML = "the value of I is " + z + ".";
+            
+        </script>
+    </body>
+</html>
